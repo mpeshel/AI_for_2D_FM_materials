0cb099724e2bd90416834b1f1a759d9957b26c53
Riya Sheth <riyasheth@Riyas-MacBook-Pro.local>
2021-03-26 11:38:36 +0800

    firstcommit

---
+++ WebInterface/app.py
@@ -0,0 +1,58 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+import json
+import math
+from pandas import DataFrame
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+model2= pickle.load(open('model2.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    print("hello")
+    meandf= pd.read_csv("averages.csv")
+    
+    str_features = [str(x) for x in request.form.values()]
+    
+    int_features=[]
+    for i in range (len(str_features)):
+        if(str_features[i]=="NA" or str_features[i]==""):
+            int_features.append(meandf.iloc[i][0])
+        else:
+            int_features.append(float(str_features[i]))
+    
+    
+           
+    final_features = [np.array(int_features)]
+    print(final_features)
+    prediction = model.predict(final_features)
+    predictproba= model.predict_proba(final_features)
+    
+
+    predictionstext= ""
+    if prediction ==1:
+        predictiontext= 'The prediction for this material is magnetic and the probability of it being magnetic is {}'.format(predictproba[0][1])
+        prediction2= model2.predict(final_features)
+        predictproba2= model2.predict_proba(final_features)
+        if prediction2==1:
+            predictiontext= predictiontext+ ' and material is predicted to be ferromagnetic and the proability of it being ferromagnetic is {}'.format(predictproba2[0][1])
+        else:
+            predictiontext= predictiontext+ ' and material is predicted to be anti-ferromagnetic and the proability of it being anti-ferromagnetic is {}'.format(predictproba[0][0])
+    else:
+        predictiontext= 'The prediction for this material is non-magnetic and the probability of it being non-magnetic is {}'.format(predictproba[0][0])
+
+    
+    return render_template('index.html', prediction_text= predictiontext)
+                         
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ WebInterface/mlmodel.py
@@ -0,0 +1,46 @@
+import sys
+import pickle
+import docx
+import json
+import pandas as pd
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import shap
+import argparse
+from pandas import DataFrame
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from pytesseract import image_to_string 
+from PIL import Image
+from sklearn import utils
+from sklearn.naive_bayes import GaussianNB
+
+df= pd.read_csv("data.csv")
+lendf= len(df.columns)
+y= df.iloc[:, -1:]
+
+x= df.drop(df.columns[lendf-1], axis=1)
+
+
+averages=[]
+for i in range(lendf-1):
+    averages.append(x.iloc[:, i].mean())
+
+meandf= DataFrame(averages)
+meandf.to_csv("averages.csv")
+
+
+
+    
+#pickle.dump(clf, open('model.pkl','wb'))
+
+
+#model = pickle.load(open('model.pkl','rb'))
+#print(clf.predict([[164, 2, 0, -4.53, 0.799, .235, -3.267]]))
+
